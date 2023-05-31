a6fd1c5d92f2675bcb53e1f6bb666be43d5a7c61
Francisco Torreblanca <francisco@apli.jobs>
2023-05-31 13:57:41 -0600

    Class 2.

---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import TodoInput from "@/components/TodoInput"
+
+function TodoForm(props) {
+    return (
+        <div>
+            <TodoInput />
+            <button>Enviar</button>
+        </div>
+    )
+}
+
+export default TodoForm
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,25 +1,42 @@
-import { useState, useEffect } from "react"
+import { SettingsContext } from "@/contexts/settingsContext"
+import { useState, useEffect, useCallback, useMemo, useContext } from "react"
 
 export default function TodoInput(props) {
     const [todo, setTodo] = useState("")
+    const [isChecked, setIsChecked] = useState(false)
+    const { darkMode } = useContext(SettingsContext)
+    console.log({ darkMode })
 
-    useEffect(() => {
-        console.log("se actualizó el TODO")
+    console.log("se renderizó el TODO")
+    const handleChange = useCallback((event) => {
+        const value = event.target.value
+        setTodo(value)
+    }, [])
+    const addTodo = useCallback(() => {
+        console.log(todo)
+        fetch("http://localhost:3000/api/todos", {
+            method: "POST",
+            body: JSON.stringify({ todo }),
+        })
+    }, [todo])
 
-        return () => {
-            console.log("se desmontó el componente")
-        }
+    const upperCaseTodo = useMemo(() => {
+        console.log("upperCaseTodo")
+        return todo.toUpperCase()
     }, [todo])
 
     return (
-        <input
-            type="text"
-            placeholder="Enter a todo item"
-            onChange={(event) => {
-                const value = event.target.value
-                setTodo(value)
-            }}
-            value={todo}
-        />
+        <>
+            <input
+                type="text"
+                placeholder="Enter a todo item"
+                onChange={handleChange}
+                value={todo}
+                style={{
+                    backgroundColor: darkMode ? "black" : "white",
+                    color: darkMode ? "white" : "black",
+                }}
+            />
+        </>
     )
 }
